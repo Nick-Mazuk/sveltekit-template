aadeea476560d6914c82a5f4ff20e0557d77c7db
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2021-04-01 00:06:53 -0700

    Add load progress indicator from ui-svelte

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -1,6 +1,9 @@
 <script>
     import '@nick-mazuk/ui-config/colors.css'
     import '@nick-mazuk/ui-config/styles.css'
+    import LoadProgressIndicator from '@nick-mazuk/ui-svelte/src/elements/load-progress-indicator.svelte'
 </script>
+
+<LoadProgressIndicator />
 
 <slot />
