6cce4338fcb2e36f2dea72ac5437cca2e8058127
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2021-04-01 00:20:06 -0700

    Fix typs

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -1,9 +1,9 @@
 <script>
     import '@nick-mazuk/ui-config/colors.css'
     import '@nick-mazuk/ui-config/styles.css'
-    import LoadProgressIndicator from '@nick-mazuk/ui-svelte/src/elements/load-progress-indicator.svelte'
+    import LoadingProgressIndicator from '@nick-mazuk/ui-svelte/src/elements/loading-progress-indicator/loading-progress-indicator.svelte'
 </script>
 
-<LoadProgressIndicator />
+<LoadingProgressIndicator />
 
 <slot />
