8aa9e481945995efec846160492fcc8bcd53719c
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2021-04-02 16:57:26 -0700

    Updates

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -1,6 +1,6 @@
 <script>
-    import '@nick-mazuk/ui-config/colors.css'
-    import '@nick-mazuk/ui-config/styles.css'
+    import '@nick-mazuk/ui-config/lib/colors.css'
+    import '@nick-mazuk/ui-config/lib/styles.css'
     import LoadingProgressIndicator from '@nick-mazuk/ui-svelte/src/elements/loading-progress-indicator/loading-progress-indicator.svelte'
 </script>
 
