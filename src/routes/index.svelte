4c2b5fe25a43b731720f76624cc4db085ffd543f
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2021-05-04 19:52:57 -0700

    Add more interesting homepage

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
 </script>
 
-<a class="link" href="https://example.com" target="_blank" rel="noreferrer">hello world</a>
+<section class="wrapper my-32 text-center">
+    <h1 class="h1">Svelte/kit starter</h1>
+    <p class="text-lg mt-8">Get started by editing <code>/src/routes/index.svelte</code></p>
+</section>
