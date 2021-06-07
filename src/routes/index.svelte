58510b87867891a8622fc2eff3236e42ee71ff10
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2021-06-07 14:53:54 -0700

    Add #main-content id to main tag

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -9,7 +9,7 @@
 
 <Seo title="" siteName="{import.meta.env.VITE_PUBLIC_SITE_NAME}" />
 
-<main>
+<main id="main-content">
     <section class="wrapper my-32 text-center">
         <h1 class="h1">Svelte/kit template</h1>
         <p class="text-lg mt-8">Get started by editing <code>/src/routes/index.svelte</code></p>
